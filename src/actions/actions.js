9d6a763c9cea96039a9d99443d86b9b1685807c6
ian.agpawa <agpawaji@gmail.com>
2020-09-19 23:26:09 -0400

    clean

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,64 +1,108 @@
 import fetch from 'cross-fetch'
 
-export const REQUEST_POSTS = 'REQUEST_POSTS'
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
-export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+// export const REQUEST_POSTS = 'REQUEST_POSTS'
+// export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+// export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+// export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
+export const FETCH_WEATHER = `FETCH_WEATHER`;
+export const FETCH_WEATHER_SUCCESS = `FETCH_WEATHER_SUCCESS`;
+export const FETCH_WEATHER_FAILURE = `FETCH_WEATHER_FAILURE`;
 
-export function selectSubreddit(subreddit) {
+export function fetchWeather() {
   return {
-    type: SELECT_SUBREDDIT,
-    subreddit
-  }
+    type: FETCH_WEATHER
+  };
 }
 
-export function invalidateSubreddit(subreddit) {
+export function fetchWeatherSuccess(payload) {
   return {
-    type: INVALIDATE_SUBREDDIT,
-    subreddit
-  }
+    type: FETCH_WEATHER_SUCCESS,
+    payload
+  };
 }
 
-function requestPosts(subreddit) {
+export function fetchWeatherFailure(payload) {
   return {
-    type: REQUEST_POSTS,
-    subreddit
-  }
+    type: FETCH_WEATHER_FAILURE,
+    payload
+  };
 }
 
-function receivePosts(subreddit, json) {
-  return {
-    type: RECEIVE_POSTS,
-    subreddit,
-    posts: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
-  }
-}
 
-function fetchPosts(subreddit) {
+// export function selectSubreddit(subreddit) {
+//   return {
+//     type: SELECT_SUBREDDIT,
+//     subreddit
+//   }
+// }
+
+// export function invalidateSubreddit(subreddit) {
+//   return {
+//     type: INVALIDATE_SUBREDDIT,
+//     subreddit
+//   }
+// }
+
+// function requestPosts(subreddit) {
+//   return {
+//     type: REQUEST_POSTS,
+//     subreddit
+//   }
+// }
+
+// function receivePosts(subreddit, json) {
+//   return {
+//     type: RECEIVE_POSTS,
+//     subreddit,
+//     posts: json.data.children.map(child => child.data),
+//     receivedAt: Date.now()
+//   }
+// }
+
+// function fetchPosts(subreddit) {
+//   return dispatch => {
+//     dispatch(requestPosts(subreddit))
+//     return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+//       .then(response => response.json())
+//       .then(json => dispatch(receivePosts(subreddit, json)))
+//   }
+// }
+// 8a4604e1c9724c6c163653f91c504862di
+function getWeather() {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=New%20York%20City&appid=8a4604e1c9724c6c163653f91c504862`
   return dispatch => {
-    dispatch(requestPosts(subreddit))
-    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then(response => response.json())
-      .then(json => dispatch(receivePosts(subreddit, json)))
+    dispatch(fetchWeather());
+    return fetch(url)
+      .then(response => {
+        console.log(response.json());
+        return response.json();
+      })
+      .then(json => dispatch(fetchWeatherSuccess(json)))
+      .catch(err => dispatch(fetchWeatherFailure(err)));
   }
 }
 
-function shouldFetchPosts(state, subreddit) {
-  const posts = state.postsBySubreddit[subreddit]
-  if (!posts) {
-    return true
-  } else if (posts.isFetching) {
-    return false
-  } else {
-    return posts.didInvalidate
-  }
-}
+// export function getWeatherDetails() {
+//   return (dispatch, getState) => {
 
-export function fetchPostsIfNeeded(subreddit) {
-  return (dispatch, getState) => {
-    if (shouldFetchPosts(getState(), subreddit)) {
-      return dispatch(fetchPosts(subreddit))
-    }
-  }
-}
+//   }
+// }
+
+// function shouldFetchPosts(state, subreddit) {
+//   const posts = state.postsBySubreddit[subreddit]
+//   if (!posts) {
+//     return true
+//   } else if (posts.isFetching) {
+//     return false
+//   } else {
+//     return posts.didInvalidate
+//   }
+// }
+
+// export function fetchPostsIfNeeded(subreddit) {
+//   return (dispatch, getState) => {
+//     if (shouldFetchPosts(getState(), subreddit)) {
+//       return dispatch(fetchPosts(subreddit))
+//     }
+//   }
+// }
