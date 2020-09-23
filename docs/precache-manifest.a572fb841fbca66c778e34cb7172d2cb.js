e277cd49a0ae604963b30c9eab539abcc4793a62
ian.agpawa <agpawaji@gmail.com>
2020-09-22 23:52:35 -0400

    build it

---
+++ docs/precache-manifest.a572fb841fbca66c778e34cb7172d2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629a4661a0e34d19991157f720e370eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08494b265adc4ae84e8",
+    "url": "/static/css/main.d447131b.chunk.css"
+  },
+  {
+    "revision": "9af6e7749ef67d237685",
+    "url": "/static/js/2.88185bfd.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.88185bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08494b265adc4ae84e8",
+    "url": "/static/js/main.275d579b.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  }
+]);
