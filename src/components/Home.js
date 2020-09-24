1a40547776c7dad183aab301169c23b8bb5bceb3
Ian Agpawa <agpawaji@gmail.com>
2020-09-23 20:16:38 -0400

    feat: Add linksg

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import CurrentWeather from './CurrentWeather';
 import ForecastWeather from './ForecastWeather';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn, faEmpire } from '@fortawesome/free-brands-svg-icons'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends Component {
 	constructor(props) { super(props); }
@@ -16,52 +19,33 @@ export default class Home extends Component {
 			<div className="site-header">
 				<div className="container">
 					<a href="index.html" className="branding">
-						<a href="https://github.com/ianagpawa/weather" target="_blank">
 						<div className="logo-type">
 							<h1 className="site-title">Weather Forecast</h1>
-							<small className="site-description hello" id="hello">Check out the code</small>
 						</div>
-						</a>
 					</a>
-
-					<div className="main-navigation">
-						<ul className="menu">
-							{/* <li className="menu-item current-menu-item"><a href="index.html">Home</a></li> */}
-							<li className="menu-item"><a href="https://ianagpawa.github.io/portfolio/" target="_blank">Portfolio</a></li>
-						</ul>
-					</div> 
-
-					<div className="mobile-navigation"></div>
-
 				</div>
 			</div>
 			
 			<CurrentWeather 
 				currentWeatherDetails={currentWeatherDetails}
-				todayForecastWeatherDetails={todayForecastWeatherDetails}
+				todayForecastWeatherDetails={todayForecastWeatherDetails} 
 			/>
 
-			<ForecastWeather
-				forecastWeatherDetails={forecastWeatherDetails}
-			/>
+			<ForecastWeather forecastWeatherDetails={forecastWeatherDetails} />
 
-			<footer class="site-footer">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8">
-							
-						</div>
-						<div class="col-md-3 col-md-offset-1">
-							<div class="social-links">
-								<a href="#"><i class="fa fa-facebook"></i></a>
-								<a href="#"><i class="fa fa-twitter"></i></a>
-								<a href="#"><i class="fa fa-google-plus"></i></a>
-								<a href="#"><i class="fa fa-pinterest"></i></a>
+			<footer className="site-footer">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3 col-md-offset-8">
+							<div className="social-links">
+								<a href="https://github.com/ianagpawa" target="_blank"><FontAwesomeIcon  icon={faGithub} /></a>
+								<a href="https://www.linkedin.com/in/ianagpawa" target="_blank"><FontAwesomeIcon  icon={faLinkedinIn} /></a>
+								<a href="https://ianagpawa.github.io/portfolio" target="_blank"><FontAwesomeIcon  icon={faEmpire} /></a>
+								<a href="https://github.com/ianagpawa/weather" target="_blank"><FontAwesomeIcon  icon={faCode} /></a>
 							</div>
 						</div>
 					</div>
-
-					<p class="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
+					<p className="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
 				</div>
 			</footer>
 		</div>
