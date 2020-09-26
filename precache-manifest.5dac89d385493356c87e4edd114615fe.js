a4b01d641ab856d4296a8d8c8eca0466009b9319
ian.agpawa <agpawaji@gmail.com>
2020-09-26 00:42:57 -0400

    build 200926

---
+++ precache-manifest.5dac89d385493356c87e4edd114615fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8616f54cc5f11b5add1e6fb0f3d827b2",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "4360bc2ffad542ef083e",
+    "url": "/weather/static/css/main.3230f710.chunk.css"
+  },
+  {
+    "revision": "1f600054ee2c1e9ab718",
+    "url": "/weather/static/js/2.5f51452d.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/weather/static/js/2.5f51452d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4360bc2ffad542ef083e",
+    "url": "/weather/static/js/main.57e2e0c2.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
